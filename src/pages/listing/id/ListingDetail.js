6fa18f93b50fce0956aa84e87a34c0e552ecf6fb
Austin Aluzzi <austinaluzzi@gmail.com>
2024-02-03 01:33:19 -0800

    Populate listing page

--- src/pages/listing/id/ListingDetail.js
+++ src/pages/listing/id/ListingDetail.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function ListingDetail() {
   const { id } = useParams(); // Get the ID from the URL
 
-  const listingDetails = {}; //fetch from id
+  const [item, setItem] = useState([]);
+  useEffect(() => {
+      const fetchItem = async () => {
+          try {
+              const response = await axios.get(`http://localhost:3001/items/${id}`);
+              setItem(response.data);
+              console.log(item);
+          } catch (err) {
+          }
+      };
+
+      fetchItem();
+  }, []);
 
   return (
     <div>
       <h2>Listing Details</h2>
-      <p>ID: {id}</p>
-      {/* Display listing details */}
+      <div>{item.title}</div>
+      <div>{item.description}</div>
     </div>
   );
 }
