db873a98d2ff058e9116f785fb6228b18a6c23ca
ethnpan07 <ethanpan@cpp.edu>
2024-02-02 19:24:32 -0800

    update

--- src/Menu.js
+++ src/Menu.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
-    <nav className="p-4 bg-gray-200 hover:w-auto w-1.5 truncate transition-all ease-out duration-300">
+    <nav className="pt-10 p-4 bg-gray-200 hover:w-auto w-1.5 truncate transition-all ease-out duration-300">
       <ul className="flex flex-col gap-7 p-3 h-full">
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/listings">Listings</Link>
+          <Link to="/listings">My Listings</Link>
         </li>
         <li className='self-baseline'>
-          <Link to="/signin">signin</Link>
+          <Link to="/signin">Sign in</Link>
         </li>
       </ul>
     </nav>
