4c4be499b797d10646503bb704a4b85be580f6b2
VietNguyen705 <vietkhoinguyen888@gmail.com>
2024-02-03 13:52:52 -0800

    stuff

--- src/Layout.js
+++ src/Layout.js
@@ -4,7 +4,7 @@ import Menu from './Menu';
 
 export default function Layout({ children }) {
   return (
-    <div className='flex h-screen'>
+    <div className='flex h-full'>
       <Menu />
       <main className="h-full w-full overflow-y-scroll">{children}</main>
     </div>
