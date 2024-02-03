59946e808b3f5ac64066606a2bacb62a5c351aaf
ethnpan07 <ethanpan@cpp.edu>
2024-02-02 18:59:36 -0800

    add button for signin/out

--- src/Layout.js
+++ src/Layout.js
@@ -6,6 +6,7 @@ export default function Layout({ children }) {
   return (
     <div className='flex h-screen'>
       <Menu />
+      
       <main className="flex w-full justify-center items-center">{children}</main>
     </div>
   );
