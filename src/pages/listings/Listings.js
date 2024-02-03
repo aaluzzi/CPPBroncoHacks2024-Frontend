69d7c63c0facab9fd8378544c34ebfade53ea5ad
Austin Aluzzi <austinaluzzi@gmail.com>
2024-02-02 19:05:32 -0800

    Populate listins page with dummy data

--- src/pages/listings/Listings.js
+++ src/pages/listings/Listings.js
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Listings() {
-    return (
-        <h1>Listings Page</h1>
-    )
+     //TODO fetch from backend properly
+    const listings = [
+        { id: 'asd23-asnnsdfb-a244', title: 'Five-Star Green Binder' },
+        { id: 'gnnSadf-adfnn-afga$Dlf', title: 'Cuetec Avid Chroma Hydra' },
+      ];
+    
+      return (
+        <div>
+          <h1 className="text-xl font-bold">Listings</h1>
+          <ul>
+            {listings.map((listing) => (
+              <li key={listing.id}>
+                <Link to={`/listings/${listing.id}`}>{listing.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
 }
